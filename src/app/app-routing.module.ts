12e8d4cafebbedf3abbe6dc6c98b5fbb70b4ff55
Phil Werli <werli@fim.uni-passau.de>
2017-05-28 10:29:15 +0200

    Add routing and update app.module
    Added imports and declarations in app.module.
    
    Added routing to the app. Routing is handled in an extra file (app-routing.module) this is imported to the app.module.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,68 @@
+import {NgModule}      from '@angular/core';
+import {Routes, RouterModule} from "@angular/router";
+
+/**
+ * Custom components, which are placed in their own directory.
+ */
+import {ServiceObjectDetailComponent} from './pages/service-object-detail/service-object-detail.component'
+import {ServiceObjectListComponent} from './pages/service-object-list/service-object-list.component'
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {ServiceObjectNotFoundComponent} from "./pages/service-object-not-found/service-object-not-found.component";
+import {SettingsComponent} from "./pages/settings/settings.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {NerosComponent} from "./pages/neros/neros.component";
+
+/**
+ * Routing configuration of the application.
+ */
+const routes: Routes = [
+    {
+        path: 'serviceobjects',
+        component: ServiceObjectListComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'settings',
+        component: SettingsComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'neros',
+        component: NerosComponent
+    },
+    {
+        path: 'so404',
+        component: ServiceObjectNotFoundComponent
+    },
+    {
+        path: 'serviceobject/:soID',
+        component: ServiceObjectDetailComponent
+    },
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,50 @@
-import {NgModule}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {NgModule}                from '@angular/core';
+import {BrowserModule}           from '@angular/platform-browser';
+import {AppComponent}            from './app.component';
+import {AppRoutingModule}        from "./app-routing.module";
+import {HttpModule}              from "@angular/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import {MdToolbarModule}         from "@angular/material"
+import {MdButtonModule}          from "@angular/material"
 
-import {AppComponent}  from './app.component';
+import {PageNotFoundComponent}          from "./pages/page-not-found/page-not-found.component";
+import {LoginComponent}                 from "./pages/login/login.component";
+import {ServiceObjectDetailComponent}   from "./pages/service-object-detail/service-object-detail.component";
+import {ServiceObjectListComponent}     from "./pages/service-object-list/service-object-list.component";
+import {DashboardComponent}             from "./pages/dashboard/dashboard.component";
+import {ServiceObjectNotFoundComponent} from "./pages/service-object-not-found/service-object-not-found.component";
+import {SeriosApiService}               from "./services/serios-api.service";
+import {SettingsComponent}              from "./pages/settings/settings.component";
+import {NerosComponent}                 from "./pages/neros/neros.component";
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        MdToolbarModule,
+        MdButtonModule,
+
+        // own modules
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        PageNotFoundComponent,
+        LoginComponent,
+        ServiceObjectDetailComponent,
+        ServiceObjectListComponent,
+        DashboardComponent,
+        ServiceObjectNotFoundComponent,
+        NerosComponent,
+        LoginComponent,
+        SettingsComponent
+    ],
+    providers: [
+        SeriosApiService
+    ],
     bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
